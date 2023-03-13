373fbb637b04bdee2e13acae63d7a40a88c04f15
gsivabalan <122631523+gsivabalan@users.noreply.github.com>
2023-03-13 12:12:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function show() {
+    let fname = document.getElementById("fname").value;
+    console.log("First Name:", fname);
+    let mname = document.getElementById("mname").value;
+    console.log("Middle Name:", mname);
+    let lname = document.getElementById("lname").value;
+    console.log("Last Name:", lname);
+    let email = document.getElementById("email").value;
+    console.log("Email:", email);
+    let password = document.getElementById("password").value;
+    console.log("Password:", password);
+  }
